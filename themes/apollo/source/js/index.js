269a6874cc644987578cd5628c156d5fcce63f9b
Xiaohang Su <sxhdragon@gmail.com>
2017-04-14 17:35:12 -0400

    fix bug

---
+++ themes/apollo/source/js/index.js
@@ -0,0 +1,10 @@
+((document)=>{
+    let imgEle = document.getElementsByClassName('logo-link')[0].children[0];
+    let icon   = document.querySelector("link[rel*='icon']");
+    setInterval(()=> {
+        let sourceStr = 'http://localhost:4000/emoji_gif_100px/emoji_'
+        + Math.floor(Math.random() * 28 + 1)  + '.gif';
+        imgEle.src = sourceStr;
+        icon.href = sourceStr;
+    }, 5000);
+})(document);
